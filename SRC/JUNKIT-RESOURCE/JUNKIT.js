99e59c5bf764f148f60a95c343da67886b516de5
Bomba <114979109+BombaUnited123@users.noreply.github.com>
2023-09-08 20:45:56 +0700

    Update JUNKIT.js

--- SRC/JUNKIT-RESOURCE/JUNKIT.js
+++ SRC/JUNKIT-RESOURCE/JUNKIT.js
@@ -1,1 +1,6 @@
-
+if onJunkitCode == (arguments[2, 892772728625282726143181919293625517, 9282614176272 : true,if,URIError,var,undefined[8],/a/, == "Fotosintensis",if return (true) => ;{
+         if JUNKIT || finally(Add.8.8682627727262.8288181817376251642)[
+                     if import ([{'@query'}]) == true;
+                     if async == "Load";
+                  ],
+}]);
