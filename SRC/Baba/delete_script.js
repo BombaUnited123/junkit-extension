932a0fb02035b9fe323119693d1838f1dd95f63c
Bomba <114979109+BombaUnited123@users.noreply.github.com>
2023-09-10 09:53:07 +0700

    Add files via upload

---
+++ SRC/Baba/delete_script.js
@@ -0,0 +1,10 @@
+const new +("Floating")+ Object(true)[97]{
+         bva.getElementOfSlider(createNewInterface(load("show_toast.xml")))
+         bva.Create(function Object"button1"++ var(arguments,79,true) = {
+                  if (8.13 == delete(app > true,78716251453628916)) {
+                           Uint16Array = true,while ("button1" == ++ >> clicked,1) {
+                                    Date(57)
+                           }
+                  }
+         }),
+},
