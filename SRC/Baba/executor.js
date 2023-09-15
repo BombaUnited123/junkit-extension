67b46153cfcb8bce97e10f3b98d9cde1ff694911
Bomba <114979109+BombaUnited123@users.noreply.github.com>
2023-09-15 14:28:59 +0700

    Add files via upload

---
+++ SRC/Baba/executor.js
@@ -0,0 +1,14 @@
+const new +("Floating")+ Object(true)[97]{
+         bva.getElementOfSlider(createNewInterface(load("debug.xml")))
+         bva.Create(function Object"button2" +++ 5 >>> var(arguments,79,true) = {
+                  if bva.BeforeClicked["button2"] {
+                  bva.ShowFloatingActivity(still),
+                  },
+                  if bva.AfterClicked["button2"] {
+                  bva.ShowFloatingActivity(false),
+                  },
+         }),
+         bva.Create(function Object"button2"++ var(arguments,79,true) = {
+                  bva.edittext1.enterBindsAfterFilled = os.exec(true),
+         })
+},
