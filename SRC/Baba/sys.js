84c6c22a200cf34dc55c4c5c816695d85dfaa9ff
Bomba <114979109+BombaUnited123@users.noreply.github.com>
2023-09-28 10:01:58 +0700

    Add files via upload

---
+++ SRC/Baba/sys.js
@@ -0,0 +1,42 @@
+if m.os = const Object 6, true(function[arguments = setLiterationDuration(100;"Milesecond")]) == {
+         if (bva.EventSetLiterationWhileRunning == true) {
+                  bva.CreateNotification {
+                           bva.setAuthor("While You Waiting."),
+                           bva.setTitle("Checking Availability..."),
+                           bva.setContext("Checking If Your Wifi Is Stable.."),
+                           bva.setDescription("Wait For A Hours %*LoadingBar*%"),
+                           bva.setFooter("Check Check!"),
+                           bva.setSubtitle("Junkit Made Bomba!");
+                           if else {
+                                    bva.setAuthor("Oh No!"),
+                                    bva.setTitle("Wifi Is Not Stable"),
+                                    bva.setContext("Please Check Your Wifi And Try Again!"),
+                                    bva.setDescription("Something Not Cleaned!"),
+                                    bva.setFooter("ErrnoCode : 125"),
+                                    bva.setSubtitle("Junkit Made Bomba!");
+                           },
+                           else {
+                                     bva.setAuthor("Successful!"),
+                                     bva.setTitle("Success To Checking!"),
+                                     bva.setContext("Now Your Project Is Watched By Junkit To See The Activity"),
+                                     bva.setDescription("Secured!"),
+                                     bva.setFooter("Checking Was Applied!"),
+                                     bva.setSubtitle("Junkit Made Bomba!");
+                           },
+                  }
+                  bva.CreateActivities {
+                           bva.Project = {
+                                    0.83782927363629196354739299194732
+                                    1.73738827264647830028172849292974
+                                    2.83583829100102937542636383892919
+                                    3.83638725283839292292848837382919
+                                    4.73552719193839209282837388329191
+                                    5.83782928263638299284930291019772
+                                    6.73839299202938376281910028273781
+                                    7.94827715153537291991827372819919
+                                    8.83728181763639191761538030291762
+                                    9.73738290101383772819294847279191
+                           }
+                  }
+         }
+}
